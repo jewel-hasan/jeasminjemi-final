58d4902254113c1082657e8fb31a735a975fd9c9
Jewel Hasan <jewelhasan.eee@gmail.com>
2024-04-22 23:55:21 +0600

    final

---
+++ style.js
@@ -0,0 +1,13 @@
+
+const activePage = window.location.pathname;
+console.log(activePage);
+const menuLinks = document.querySelectorAll('li a');
+
+for (const link of menuLinks){
+    if(link.href.includes(`${activePage}`)){
+        console.log(`${activePage}`)
+        link.classList.add('menu-active');
+        link.classList.remove('menu-style');
+    }
+
+}
